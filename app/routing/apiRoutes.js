3fff5d5bd6560990be18b77308b6e92a2fbc3a09
Michael Bailey <michaeljaybailey@gmail.com>
2020-09-22 20:25:22 -0700

    added server.js and set up gitignore

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,59 @@
+
+var friends = require("../data/friends");
+
+
+module.exports = function(app) {
+ 
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+
+  
+
+  app.post("/api/friends", function(req, res) {
+    
+    var bestMatch = {
+      name: "",
+      photo: "",
+      friendDifference: Infinity
+    };
+
+    
+    var userData = req.body;
+    var userScores = userData.scores;
+
+   
+    var Difference;
+
+   
+    for (var i = 0; i < friends.length; i++) {
+      var currentFriend = friends[i];
+      Difference = 0;
+
+      console.log(currentFriend.name);
+
+      for (var j = 0; j < currentFriend.scores.length; j++) {
+        var currentFriendScore = currentFriend.scores[j];
+        var currentUserScore = userScores[j];
+
+        
+        Difference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
+      }
+
+     
+      if (Difference <= bestMatch.friendDifference) {
+        
+        bestMatch.name = currentFriend.name;
+        bestMatch.photo = currentFriend.photo;
+        bestMatch.friendDifference = totalDifference;
+      }
+    }
+
+    
+    friends.push(userData);
+
+   
+    res.json(bestMatch);
+  });
+};
